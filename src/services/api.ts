2bba53efcc815d36323ebf5120f875948e51594c
Keerthanaa <vijayaaakeerthanaa@gmail.com>
2026-08-30 20:54:19 +0530

    Update frontend API URL for deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://taskflow-gaiw.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 // Automatically attach JWT token
 api.interceptors.request.use(
   (config) => {
@@ -15,10 +13,8 @@ api.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
   },
   (error) => Promise.reject(error)
 );
-
 export default api;
